5be9cd1d84fb17254e43e19417b26f2a31d7a88f
Hendi Santika <hendisantika@gmail.com>
2021-12-30 06:43:13 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendisantika/SpringBootKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
