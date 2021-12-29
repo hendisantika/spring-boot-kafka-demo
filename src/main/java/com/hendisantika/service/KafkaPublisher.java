8a8de5c1b176994dfe0dff3ed7a42c98f840a2f5
Hendi Santika <hendisantika@gmail.com>
2021-12-30 06:50:59 +0700

    #10 - Add publishBooksUpdateMessage in KafkaPublisher

--- src/main/java/com/hendisantika/service/KafkaPublisher.java
+++ src/main/java/com/hendisantika/service/KafkaPublisher.java
@@ -1,6 +1,7 @@
 package com.hendisantika.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.model.Books;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,4 +50,27 @@ public class KafkaPublisher {
         });
     }
 
+    public void publishBooksUpdateMessage(Books books, String topicName) {
+        StringBuilder jsonStr = new StringBuilder();
+        try {
+            jsonStr.append(obj.writeValueAsString(books));
+            LOG.info("Rest API to publish a message to kafka topic {}", jsonStr);
+        } catch (Exception ex) {
+            LOG.error("Exception occurred", ex);
+        }
+
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, jsonStr.toString());
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                LOG.info("Sent message=[ {} ] with offset=[ {} ]", jsonStr, result.getRecordMetadata().offset());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                LOG.info("Unable to send message=[ {} ] due to : {}", jsonStr, ex.getMessage());
+            }
+        });
+    }
 }
